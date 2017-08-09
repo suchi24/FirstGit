a9b2e2ca6be09e464ebb42014b54c6c8b49c98a5
KumFamily <kk.suchithra@gmail.com>
2017-08-09 11:41:35 -0700

    saved binary search program

--- src/firstA.java
+++ src/firstA.java
@@ -4,9 +4,8 @@ public class firstA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git test");
-		System.out.println("Git end");
-		System.out.println("Verify");
 		
+		System.out.println("Git end");
 		
 
 	}	
